a5ac6056c9a6f7955556ac0667e738ed1213009f
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2022-09-07 08:18:50 +0200

    initial

---
+++ practice_site/src/main/java/com/javapointers/Controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.javapointers.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String viewHome(){
+        return "index";
+    }
+}
